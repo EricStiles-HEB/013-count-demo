0589aa46535e2f3e6b81d8ccf06802be101eb549
Kyle Ruddy <kmruddy@users.noreply.github.com>
2020-07-27 16:54:51 -0400

    Update main.tf
    Updating for terraform block input of required proviers

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "2.70.0"
+      alias = hashi
+    }
+  }
+}
+
 module "s3-module" {
   source = "app.terraform.io/TFTMM/s3-module/aws"
   providers = {
-    aws = hashicorp/aws
+    aws = aws.hashi
   }
   count = 5
 }

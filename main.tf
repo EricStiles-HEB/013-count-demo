16e507497231a2f349310230869be3de096aff0b
Kyle Ruddy <kmruddy@users.noreply.github.com>
2020-07-27 16:52:26 -0400

    Update main.tf
    Update provider input

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "s3-module" {
   source = "app.terraform.io/TFTMM/s3-module/aws"
   providers = {
-    aws = "registry.terraform.io/hashicorp/aws"
+    aws = "hashicorp/aws"
   }
   count = 5
 }

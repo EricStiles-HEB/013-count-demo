6e16088ca0ff16e01096ed4b888b9eb3400f45d9
Kyle Ruddy <kmruddy@users.noreply.github.com>
2020-07-27 16:33:42 -0400

    Update main.tf
    Updating providers block

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 module "s3-module" {
-  source  = "app.terraform.io/TFTMM/s3-module/aws"
-  providers = { aws }
+  source = "app.terraform.io/TFTMM/s3-module/aws"
+  providers = {
+    aws = "aws"
+  }
   count = 5
 }
